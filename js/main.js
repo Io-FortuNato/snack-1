58e40a8828f8e16ee8d850eb855ca6e18ac3dea1
io.FortuNato <nomefortunato60@gmail.com>
2026-02-05 10:36:25 +0100

    js-snacks-blocco-1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = Number(prompt("inserire un numero"));
+let secondNumber = Number(prompt("inserire un numero successivo a quello messo in precedenza"));
+let totale = firstNumber + secondNumber
+
+if (firstNumber < secondNumber) {
+    console.log(">");
+    
+}
